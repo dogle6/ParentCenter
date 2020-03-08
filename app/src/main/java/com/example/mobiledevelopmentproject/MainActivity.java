35bb5129ad0a6502b937b3a8ed7ab98d2bfe6e0c
GrahamCaldwell <graham.caldwell99@gmail.com>
2020-03-07 19:23:12 -0500

    Test Push

--- app/src/main/java/com/example/mobiledevelopmentproject/MainActivity.java
+++ app/src/main/java/com/example/mobiledevelopmentproject/MainActivity.java
@@ -151,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                     updateUI(user);
                     getUserDate(db, email);
                     // TODO: Switch intent to home page
-                    //Test
+                    // Test Push
 
                 } else{
                     // If sign in fails, display a failure message to the user
